2da0e9d81a00689a1ef26fcfcf6012560cdff29e
SinghAman21 <singhaman1825@gmail.com>
2026-09-06 19:47:27 +0530

    feat(analytics): vercel analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import MainMenu from "./pages/MainMenu.js";
 import LocalPlay from "./pages/LocalPlay.js";
 import CreateRoom from "./pages/CreateRoom.js";
@@ -8,13 +9,16 @@ import OnlineGame from "./pages/OnlineGame.js";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainMenu />} />
-      <Route path="/local" element={<LocalPlay />} />
-      <Route path="/create-room" element={<CreateRoom />} />
-      <Route path="/join-room" element={<JoinRoom />} />
-      <Route path="/room/:roomId" element={<RoomLobby />} />
-      <Route path="/online/:roomId" element={<OnlineGame />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<MainMenu />} />
+        <Route path="/local" element={<LocalPlay />} />
+        <Route path="/create-room" element={<CreateRoom />} />
+        <Route path="/join-room" element={<JoinRoom />} />
+        <Route path="/room/:roomId" element={<RoomLobby />} />
+        <Route path="/online/:roomId" element={<OnlineGame />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
